14da2d055467cc7e8520d296be6ba7eaa853d096
Ian Douglas Scott <ian@iandouglasscott.com>
2018-06-12 11:06:39 -0700

    Better design

--- pdfmanager.py
+++ pdfmanager.py
@@ -3,9 +3,10 @@
 import sqlite3
 import os
 from enum import Enum
-import sys
 import collections
 import subprocess
+import readline
+import shlex
 
 PDF_VIEWER = 'zathura'
 
@@ -14,6 +15,7 @@ class Status(Enum):
     read = 1
 
 Entry = collections.namedtuple("Entry", "path title subject status")
+Result = collections.namedtuple("Result", "type items")
 
 class Database:
     def __init__(self, path):
@@ -42,43 +44,96 @@ class Database:
         for i in c:
             yield Entry(*i)
 
+    def subjects(self):
+        c = self.conn.cursor()
+        c.execute('SELECT DISTINCT subject FROM files')
+        for i in c:
+            yield i[0]
+
     def close(self):
         self.conn.close()
 
 
-db = Database('list.db')
-items = None
+def command_decorator(commands):
+    def command(name):
+        def wrapper(func):
+            if isinstance(name, str):
+                commands[name] = func
+            else:
+                for i in name:
+                    commands[i] = func
+            return func
+        return wrapper
+    return command
 
-while True:
-    line = input('> ').split()
 
-    if len(line) == 0:
-        pass
-    elif line[0] == 'add':
-        if len(line) != 4:
+class PDFManager:
+    commands = {}
+    command = command_decorator(commands)
+
+    def __init__(self):
+        readline.parse_and_bind('')
+        self.db = Database('list.db')
+        self.result = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.db.close()
+
+    @command('add')
+    def add(self, *args):
+        if len(args) != 3:
             print("add <file> <title> <subject>")
-        path, title, subject = line[1:]
+        path, title, subject = args
+
         if not os.path.exists(path):
             print(f"No such file: {path}")
         db.insert(path, title, subject, Status.unread)
-    elif line[0] == 'ls':
+
+    @command(['s', 'subjects'])
+    def list_subjects(self):
+        self.result = Result('subjects', list(self.db.subjects()))
+        for x, i in enumerate(self.result.items):
+            print(f"[{x}] {i}")
+
+    @command('ls')
+    def list_pdfs(self, *args):
         subject = None
-        if len(line) > 1:
-            subject = line[1]
-        items = list(db.list(subject))
-        for x,i in enumerate(items):
+        if len(args) > 0:
+            subject = args[0]
+
+        self.result = Result('pdfs', list(self.db.list(subject)))
+        for x, i in enumerate(self.result.items):
             print(f"[{x}] {i.title}")
-    elif line[0] in ('q', 'quit'):
-        sys.exit()
-    elif line[0].isnumeric():
-        idx = int(line[0])
-        if items is None:
+
+    def select(self, idx):
+        if self.result is None:
             print("No search")
-        elif idx < 0 or idx >= len(items):
+        elif idx < 0 or idx >= len(self.result.items):
             print(f"Index '{idx}' out of bounds")
         else:
-            subprocess.call([PDF_VIEWER, items[idx].path])
-    else:
-        print(f"No such command: '{line[0]}'")
+            item = self.result.items[idx]
+            if self.result.type == 'pdfs':
+                subprocess.call([PDF_VIEWER, item.path])
+            elif self.result.type == 'subjects':
+                self.list_pdfs(item)
 
-db.close()
+    def loop(self):
+        while True:
+            line = shlex.split(input('> '))
+
+            if len(line) == 0:
+                pass
+            elif line[0] in self.commands:
+                self.commands[line[0]](self, *line[1:])
+            elif line[0].isnumeric():
+                self.select(int(line[0]))
+            elif line[0] in ('q', 'quit'):
+                break
+            else:
+                print(f"No such command: '{line[0]}'")
+
+with PDFManager() as pdfmanager:
+    pdfmanager.loop()
